a7f6ec7267e1b300cf0a81982e4c204e1c35e45a
tasty-gumi <1021989072@qq.com>
2024-10-30 20:06:17 +0800

    fix:remove some file change for dev
    Signed-off-by: tasty-gumi <1021989072@qq.com>

--- rebuild.sh
+++
@@ -1,8 +0,0 @@
-#! bin/bash
-rm /home/adam/milvus/cmake_build/bin/geos-config
-make -j8
-pushd deployments/docker/dev/
-docker compose down
-sudo rm -rf ./volumes/*
-docker compose up -d
-popd
--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -25,8 +25,7 @@ done
 ROOT_DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
 PROTO_DIR=$ROOT_DIR/internal/proto/
-# API_PROTO_DIR=$ROOT_DIR/cmake_build/thirdparty/milvus-proto/proto
-API_PROTO_DIR=/home/adam/milvus-proto/proto
+API_PROTO_DIR=$ROOT_DIR/cmake_build/thirdparty/milvus-proto/proto
 CPP_SRC_DIR=$ROOT_DIR/internal/core
 PROTOC_BIN=$ROOT_DIR/cmake_build/bin/protoc
 
